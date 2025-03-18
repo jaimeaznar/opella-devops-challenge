c9aa8255c26eecdc84d321f8d427299e9c1e5a55
jaimeaznar <jaimeaznarupv@gmail.com>
2025-03-18 19:08:32 +0100

    vnsetup: adding subnet

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -6,3 +6,26 @@ resource "azurerm_virtual_network" "vnet" {
   dns_servers         = var.dns_servers
   tags                = var.tags
 }
+
+resource "azurerm_subnet" "subnet" {
+  for_each             = var.subnets
+  name                 = each.key
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = each.value.address_prefixes
+  service_endpoints    = each.value.service_endpoints
+
+  dynamic "delegation" {
+    for_each = each.value.delegation != null ? each.value.delegation : {}
+    content {
+      name = delegation.key
+      dynamic "service_delegation" {
+        for_each = delegation.value
+        content {
+          name    = service_delegation.value.name
+          actions = lookup(service_delegation.value, "actions", null)
+        }
+      }
+    }
+  }
+}
