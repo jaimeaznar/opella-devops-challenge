15edc6c42710c9f26e06d59d598f9a25b3ffccff
jaimeaznar <jaimeaznarupv@gmail.com>
2025-03-19 00:30:39 +0100

    terraform: updating storage account name

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -5,7 +5,7 @@ provider "azurerm" {
 terraform {
   backend "azurerm" {
     resource_group_name  = "terraform-state-rg"
-    storage_account_name = "opellaterraformstateinterviewjaime"
+    storage_account_name = "opellaterraformstate"
     container_name       = "tfstate"
     key                  = "dev.terraform.tfstate"
   }
