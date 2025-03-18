28b890fc6e319836f64234e99f0560fe4c3b7d14
jaimeaznar <jaimeaznarupv@gmail.com>
2025-03-18 23:37:31 +0100

    terraform: creating terraform workflow

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -38,3 +38,72 @@ module "vnet" {
     Environment = "Production"
   })
 }
+
+
+resource "azurerm_network_interface" "vm_nic" {
+  name                = "${var.prefix}-vm-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = module.vnet.subnet_ids["app"]
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "${var.prefix}-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.vm_nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file(var.ssh_public_key_path)
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  tags = merge(var.tags, {
+    Environment = "Production"
+  })
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "${var.prefix}storage"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  network_rules {
+    default_action             = "Deny"
+    virtual_network_subnet_ids = [module.vnet.subnet_ids["app"]]
+    bypass                     = ["AzureServices"]
+  }
+
+  tags = merge(var.tags, {
+    Environment = "Production"
+  })
+}
+
+resource "azurerm_storage_container" "container" {
+  name                  = "data"
+  storage_account_id    = azurerm_storage_account.storage.id
+  container_access_type = "private"
+}
