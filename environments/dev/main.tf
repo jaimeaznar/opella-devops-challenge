57eba007a2bb0fa4ce886d992398d2187e253f3d
jaimeaznar <jaimeaznarupv@gmail.com>
2025-03-18 21:54:29 +0100

    nv-setup: adding provider info

---
+++ environments/dev/main.tf
@@ -0,0 +1,19 @@
+provider "azurerm" {
+  features {}
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "terraform-state-rg"
+    storage_account_name = "opellaterraformstate"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
