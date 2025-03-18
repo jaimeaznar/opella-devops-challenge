c242177e3e5af765d546688044b8bbdcd8d5bfd7
jaimeaznar <jaimeaznarupv@gmail.com>
2025-03-18 19:09:52 +0100

    vnsetup: adding nsg

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -29,3 +29,37 @@ resource "azurerm_subnet" "subnet" {
     }
   }
 }
+
+resource "azurerm_network_security_group" "nsg" {
+  for_each            = var.subnets
+  name                = "${each.key}-nsg"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  tags                = var.tags
+
+  # security rule - HTTP
+  security_rule {
+    name                       = "AllowHTTP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  # security rule - SSH
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
