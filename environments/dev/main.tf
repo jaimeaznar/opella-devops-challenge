8f4b534bca2d1524c3426aa47ece8ad6e8e0e40f
jaimeaznar <jaimeaznarupv@gmail.com>
2025-03-19 10:02:33 +0100

    terraform: fixing issue with ssh and storage name

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -18,6 +18,12 @@ terraform {
   }
 }
 
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
@@ -84,7 +90,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "${var.prefix}storage"
+  name                     = "devopella${random_string.suffix.result}"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
